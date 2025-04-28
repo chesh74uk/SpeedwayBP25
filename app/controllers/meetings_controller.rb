fac5e88acba08044fb85a627da989bdef1468903
chesh <chesh@chesh.info>
2025-04-28 17:45:47 +0100

    added CRUD meetings

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,5 +1,52 @@
 class MeetingsController < ApplicationController
+  before_action :set_meeting, only: %i[show edit update destroy]
   def index
-    @meetings=Meeting.all
+    @meetings = Meeting.all.order(date: :desc)
+  end
+
+  def new
+    @meeting = Meeting.new
+  end
+
+  def edit
+  end
+
+  def create
+    @meeting = Meeting.new(meeting_params)
+    respond_to do |format|
+      if @meeting.save
+        format.html { redirect_to meetings_path notice: "Meeting was successfully created" }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @meeting.update(meeting_params)
+        format.html { redirect_to meetings_path, notice: "Meeting was successfully updated" }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+  def show
+  end
+
+  def destroy
+    @meeting.destroy
+    respond_to do |format|
+      format.html { redirect_to meetings_url, notice: "Meeting was successfully removed" }
+    end
+  end
+
+  private
+
+  def set_meeting
+    @meeting = Meeting.find(params[:id])
+  end
+  def meeting_params
+    params.expect(meeting: [ :date, :home_team, :away_team, :gate_choice ])
   end
 end
--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,2 +1,3 @@
 class Meeting < ApplicationRecord
+  validates :home_team, :away_team, :date, :presence => true
 end
