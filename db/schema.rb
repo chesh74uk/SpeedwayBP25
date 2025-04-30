def04361b16ab600e9ce119d41dd730a051ab648
chesh <chesh@chesh.info>
2025-04-30 16:55:39 +0100

    added team model and seed db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_28_154313) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_30_151859) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -30,6 +30,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_28_154313) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "rider"
+    t.string "team_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'teams.csv'))
+csv = CSV.parse(csv_text, headers: true, encoding: 'ISO-8859-1')
+csv.each do |row|
+  t = Team.new
+  t.rider = row['rider']
+  t.team_name = row['team_name']
+  t.save
+  puts "#{t.rider}, #{t.team_name} saved"
+end
+puts "There are now #{Team.count} rows in the team table"
